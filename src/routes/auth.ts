4a383c2b5571dac7c6cd5bbe94d46a8a6b8f28c4
bozinovskidaniel <bozinovskidaniel@hotmail.com>
2021-09-05 08:23:42 +1000

    getting setup

---
+++ src/routes/auth.ts
@@ -0,0 +1,30 @@
+import { Request, Response, Router } from "express";
+
+import { User } from './../entities/User';
+
+const register = async (req: Request, res: Response) => {
+    const {email, username, password} = req.body;
+
+    try {
+
+        // TODO: Validate data
+
+        // TODO: Create User
+        const user = new User({email, username, password})
+        await user.save() // Save to db
+
+        // TODO: Return User
+        return res.json(user)
+
+    } catch (err) {
+
+        console.log(err)
+        return res.status(500).json(err)
+    }
+
+} 
+
+const router = Router();
+router.post('/register', register)
+
+export default router;

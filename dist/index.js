d9d01bd5a4d567926db886c876c4f1cbc7898548
bozinovskidaniel <bozinovskidaniel@hotmail.com>
2021-09-04 20:31:55 +1000

    got typescript and node working together, got base files created

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core_1 = require("@mikro-orm/core");
+const constants_1 = require("./constants");
+const Post_1 = require("./entities/Post");
+const main = async () => {
+    const orm = await core_1.MikroORM.init({
+        entities: [Post_1.Post],
+        dbName: 'redundit',
+        type: 'postgresql',
+        debug: !constants_1.__prod__
+    });
+    const post = orm.em.create(Post_1.Post, { title: "My First post" });
+    await orm.em.persistAndFlush(post);
+};
+main();
+//# sourceMappingURL=index.js.map

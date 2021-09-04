e09743416f269276ff88bd15ceec4e313351bb1e
bozinovskidaniel <bozinovskidaniel@hotmail.com>
2021-09-05 08:50:32 +1000

    added password hasing, also excluding it from json return when creating a user

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,4 +1,8 @@
-import {Entity, PrimaryGeneratedColumn, Column, BaseEntity, Index} from "typeorm";
+import { IsEmail, Length } from "class-validator";
+import {Entity, PrimaryGeneratedColumn, Column, BaseEntity, Index, CreateDateColumn, UpdateDateColumn, BeforeInsert} from "typeorm";
+
+import bcrypt from "bcrypt"
+import {classToPlain, Exclude} from "class-transformer"
 
 @Entity("users")
 export class User extends BaseEntity {
@@ -8,18 +12,38 @@ export class User extends BaseEntity {
         Object.assign(this, user)
     }
 
+    @Exclude()
     @PrimaryGeneratedColumn()
     id: number;
 
     @Index()
+    @IsEmail()
     @Column({unique: true})
     email: string;
 
     @Index()
+    @Length(3, 255, {message: 'Username must be atleast 3 characters long'})
     @Column({unique: true})
     username: string;
 
+    @Exclude()
+    @Length(6)
     @Column()
     password: string;
+
+    @CreateDateColumn()
+    createdAt: Date 
+
+    @UpdateDateColumn()
+    updatedAt: Date
+
+    @BeforeInsert()
+    async hashPassword() {
+        this.password = await bcrypt.hash(this.password, 6)
+    }
+
+    toJSON(){
+        return classToPlain(this)
+    }
 
 }
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,6 +1,8 @@
 import { Request, Response, Router } from "express";
 
+
 import { User } from './../entities/User';
+import { validate } from "class-validator";
 
 const register = async (req: Request, res: Response) => {
     const {email, username, password} = req.body;
@@ -8,9 +10,23 @@ const register = async (req: Request, res: Response) => {
     try {
 
         // TODO: Validate data
+        let errors: any = {}
+        const emailUser = await User.findOne({email})
+        const usernameUser = await User.findOne({username})
+
+        if (emailUser) errors.email = "Email is already taken"
+        if (usernameUser) errors.username = "Username is already taken"
+
+        if (Object.keys(errors).length > 0) {
+            return res.status(400).json(errors)
+        }
 
         // TODO: Create User
         const user = new User({email, username, password})
+        errors = await validate(user)
+
+        if (errors.length > 0) return res.status(400).json(errors)
+        
         await user.save() // Save to db
 
         // TODO: Return User

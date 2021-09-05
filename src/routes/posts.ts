2dfad7dfd5b334dfc5a2a8e53a2c2a940e645060
bozinovskidaniel <bozinovskidaniel@hotmail.com>
2021-09-05 17:55:22 +1000

    created fetching all posts and a single post, wrking on comments now

--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -31,7 +31,45 @@ const createPost = async (req: Request, res: Response) => {
 
 }
 
+const getPosts = async (_: Request, res: Response) => {
+    
+    try {
+        // Order by created at
+        const posts = await Post.find({
+            order: {createdAt: 'DESC'}
+        })
+
+        return res.json(posts)
+    } catch (err) {
+        console.log(err)
+        return res.status(500).json({error: "Something went wrong"})
+    }
+
+}
+
+const getPost = async (req: Request, res: Response) => {
+    
+    const {identifier, slug} = req.params 
+
+    try {
+        // Order by created at
+        const post = await Post.findOneOrFail({ identifier, slug}, {
+            relations: ['sub']
+        })
+
+        return res.json(post)
+    } catch (err) {
+        console.log(err)
+        return res.status(404).json({error: "Post not found"})
+    }
+
+}
+
+
+
 const router = Router()
 router.post('/', auth, createPost)
+router.get('/', getPosts)
+router.get('/:identifier/:slug', getPost)
 
 export default router;
